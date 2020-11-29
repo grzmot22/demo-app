f14cfc5bb200c71114501a99f08fa0d27a1bbd9c
Grzmot22 <grzmot22@gmail.com>
2020-11-29 04:51:20 +0000

    Add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCtN7oPLgQF6EbSGdLmrpkcllnPUkM-pfM",
+    authDomain: "test-app-f3dac.firebaseapp.com",
+    databaseURL: "https://test-app-f3dac.firebaseio.com",
+    projectId: "test-app-f3dac",
+    storageBucket: "test-app-f3dac.appspot.com",
+    messagingSenderId: "757905477962",
+    appId: "1:757905477962:web:e85dcb1df9620277cdb1b3",
+    measurementId: "G-6V8KYR2D9C"
+  };
+
+firebase.initializeApp(firebaseConfig);
